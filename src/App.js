828859cae6d9a9f99cbffd87075355ff8c6bd8b3
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-12-15 21:05:06 +0530

    Add colorbox and style the pallete and colorbox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 
-
 const { Component } = require("react");
 class App extends Component{
   render(){
     return(
       <div>
-        <Palette {...seedColors[7]} />
+        <Palette {...seedColors[6]} />
       </div>
     )
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 export default class Palette extends Component {
     render() {
+        const colorBoxes=this.props.colors.map((color)=>{
+            return <ColorBox background={color.color} name={color.name}/>
+        })
         return (
-            <div>
-                <h1>Palette</h1>
+            <div className="Palette">
+                {/* //Navbar goes here */}
+                <div className="Palette-colors">
+                    {/* //Bunch of color boxes */}
+                    {colorBoxes}
+                </div>
+                {/* //Footer */}
             </div>
         )
     }
