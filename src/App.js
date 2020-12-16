c1cf9d8d47b6d32227970738f9a3bf73f4a1b37b
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-12-16 22:33:59 +0530

    Add coloHelper file to generate colors of various shade

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelper";
 
 const { Component } = require("react");
 class App extends Component{
   render(){
+    console.log(generatePalette(seedColors[4]));
     return(
       <div>
         <Palette {...seedColors[6]} />
