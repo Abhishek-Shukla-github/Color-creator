ad657e23092a6253be216f44001e40fe39deb8dd
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-12-21 01:44:34 +0530

    Add Main Palette Route to the MiniPalette Component

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -40,7 +40,7 @@ const styles = {
     display:"inline-block",
     margin:"0 auto",
     position:"relative",
-    marginBottom:"-3.5px",
+    marginBottom:"-4px",
   }
 };
 
@@ -48,12 +48,11 @@ function MiniPalette(props) {
   const { classes, paletteName, emoji,id,colors } = props;
   const miniColorBoxes=colors.map((color)=>(<div className={classes.miniColor} key={color.name} style={{backgroundColor:color.color}}/>))
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors} >{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
-      <Link to={`/palette/${id}`}>{paletteName}</Link>
     </div>
   );
 }
